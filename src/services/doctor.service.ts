855a57d288d40e061edeaf8e20c2bac5de4b992c
qbentil <bentilshadrack72@gmail.com>
2025-02-14 16:40:18 +0000

    Add doctor services

--- src/services/doctor.service.ts
+++ src/services/doctor.service.ts
@@ -1,5 +1,7 @@
-import { DOCTOR_MODEL } from '../models';
-import { Types } from 'mongoose';
+import { DOCTOR_MODEL, PATIENT_MODEL } from '../models';
+import { QueryOptions, Types } from 'mongoose';
+
+import { IPatientModel } from '../types';
 import { __generateCode } from '../helpers';
 
 export const initDoctor = async (user: Types.ObjectId) => {
@@ -13,3 +15,99 @@ export const initDoctor = async (user: Types.ObjectId) => {
     throw new Error(error);
   }
 };
+
+
+export const updateDoctor = async (id: Types.ObjectId, query: QueryOptions) => {
+  try {
+    const doctor = await DOCTOR_MODEL.findByIdAndUpdate(id, query, { new: true });
+    return doctor;
+  }
+  catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export const getDoctor = async (query: QueryOptions) => {
+  try {
+    const doctor = await DOCTOR_MODEL.findOne(query)
+      .populate('user', 'email name')
+      .populate('patients', 'name email');
+    return doctor;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export const getDoctors = async (query: QueryOptions) => {
+  try {
+    const doctors = await DOCTOR_MODEL.find(query)
+      .populate('user', 'email name')
+      .populate('patients', 'name email');
+    return doctors;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+
+export const AddPatientMedicalHistory = async (patientId: Types.ObjectId, history: string) => {
+  try {
+    const patient = await PATIENT_MODEL.findOne<IPatientModel>({
+      _id: patientId
+    });
+    if (!patient) {
+      throw new Error('Patient not found');
+    }
+    patient.addMedicalHistory(history);
+    return patient;
+  }
+  catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export const ReadPatientMedicalHistory = async (patientId: Types.ObjectId) => {
+  try {
+    const patient = await PATIENT_MODEL.findOne<IPatientModel>({
+      _id: patientId
+    });
+    if (!patient) {
+      throw new Error('Patient not found');
+    }
+    return patient.medicalHistory;
+  }
+  catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export const AddPatientNotes = async (patientId: Types.ObjectId, notes: string) => {
+  try {
+    const patient = await PATIENT_MODEL.findOne<IPatientModel>({
+      _id: patientId
+    });
+    if (!patient) {
+      throw new Error('Patient not found');
+    }
+    patient.addNote(notes);
+    return patient;
+  }
+  catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export const ReadPatientNotes = async (patientId: Types.ObjectId) => {
+  try {
+    const patient = await PATIENT_MODEL.findOne<IPatientModel>({
+      _id: patientId
+    });
+    if (!patient) {
+      throw new Error('Patient not found');
+    }
+    return patient.notes;
+  }
+  catch (error: any) {
+    throw new Error(error);
+  }
+}
